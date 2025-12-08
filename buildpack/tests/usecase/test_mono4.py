025ca401bead33605aceb0272377d2e78eb0b223
Telping <tarig.elomari@gmail.com>
2025-12-08 10:45:02 +0100

    Uploading the balena project to personal git account

---
+++ buildpack/tests/usecase/test_mono4.py
@@ -0,0 +1,15 @@
+import basetest
+
+
+class TestCaseMono4(basetest.BaseTest):
+    def setUp(self):
+        super().setUp()
+        self.setUpCF(
+            "MontBlancApp720.mpk",
+            env_vars={"DEPLOY_PASSWORD": self.mx_password},
+        )
+        self.startApp()
+
+    def test_mono4(self):
+        self.assert_app_running()
+        self.assert_string_in_recent_logs("Selecting Mono Runtime: mono-4")
